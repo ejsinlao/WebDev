cbd5de83b94e82652d870d7c53ba97e9a3f809a1
Mustafa Malick <mustafa.malick@ryerson.ca>
2018-12-01 23:01:44 -0500

    AJAX bug in eventApplication

--- app/assets/javascripts/components/event_application.js.jsx
+++ app/assets/javascripts/components/event_application.js.jsx
@@ -1,14 +1,35 @@
 class EventApplication extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { events: [] };
+  }
+
+  componentDidMount() {
+    this.getDataFromApi();
+  }
+
+  getDataFromApi() {
+    var self = this;
+    $.ajax({
+      url: '/api/events',
+      success: function(data) {
+        self.setState({ events: data });
+      }
+
+    });
+  }
+
   render() {
     return(
       <div className="container">
         <div className="jumbotron">
-          <h1>Ryerson Table Tennis Events Log</h1>
+          <h1>Table Tennis Events Log</h1>
           <p>CPS530</p>
         </div>
         <div className="row">
           <div className="col-md-12">
-            <EventTable />
+            <EventTable events={this.state.events} />
           </div>
         </div>
       </div>
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+1.upto(10) do |i|
+  Event.create(name: "Tournament #{i}",
+               description: "regional tournament within Ontario #{i}",
+               event_date: Date.today + rand(3).months,
+               place: "Waterloo #{i}")
+end

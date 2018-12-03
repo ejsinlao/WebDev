4638e917187692af1ea98539d86d664dcfa61f43
Darren Mei <Darrenmei1996@gmail.com>
2018-12-02 21:10:48 -0500

    Undo add change

--- app/assets/javascripts/components/new_form.js.jsx
+++ app/assets/javascripts/components/new_form.js.jsx
@@ -18,7 +18,7 @@ var NewForm = React.createClass({
     var self = this;
     if (this.validForm()) {
       $.ajax({
-        url: '/api/events/create',
+        url: '/api/events',
         method: 'POST',
         data: { event: self.state },
         success: function(data) {
